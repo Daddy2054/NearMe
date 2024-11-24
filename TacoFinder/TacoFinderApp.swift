bf53180fe1efeca68255b4b1aaccd6e0ff2e5565
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-24 22:39:32 +0100

    Initial Commit

---
+++ TacoFinder/TacoFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacoFinderApp.swift
+//  TacoFinder
+//
+//  Created by Jean on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TacoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
